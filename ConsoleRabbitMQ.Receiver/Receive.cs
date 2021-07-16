1596f6f3049f92217877c4377f34fb9ce20fe9f6
Суханкин Владимир Юрьевич <vysukha1@mts.ru>
2021-07-16 12:10:30 +0400

    Настройка connection с cloudamqp, настройка репозитория, проверка прохождения сообщений через тестовую очередь

---
+++ ConsoleRabbitMQ.Receiver/Receive.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsoleRabbitMQ.Recivier
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bublpzgh:M7fqfVEcZeB_LeBPkXanh8ZajkyGxfHq@snake.rmq2.cloudamqp.com/bublpzgh");
+            //factory.RequestedHeartbeat = 15;
+            factory.AutomaticRecoveryEnabled = true;
+            //var connection = factory.CreateConnection();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+}
---
+++ СonsoleRabbitMQ.Sender/Sender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ConsoleRabbitMQ.Sender
+{
+    class Send
+    {
+        public static void Main()
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bublpzgh:M7fqfVEcZeB_LeBPkXanh8ZajkyGxfHq@snake.rmq2.cloudamqp.com/bublpzgh");
+            //factory.RequestedHeartbeat = 15;
+            factory.AutomaticRecoveryEnabled = true;
+            //var connection = factory.CreateConnection();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
