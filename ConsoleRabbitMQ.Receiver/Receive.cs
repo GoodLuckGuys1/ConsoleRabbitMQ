8111876d720189fd058956755327431a50574135
Суханкин Владимир Юрьевич <vysukha1@mts.ru>
2021-10-01 09:57:53 +0400

    финальная доработка сервиса

--- ConsoleRabbitMQ.Receiver/Receive.cs
+++ ConsoleRabbitMQ.Receiver/Receive.cs
@@ -1,41 +1,93 @@
-﻿using RabbitMQ.Client;
+﻿using ConsoleRabbitMQ.Receiver;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleRabbitMQ.Recivier
 {
     class Program
     {
+        private static IConnection connection;
+        private static IModel channelPost;
+        private static IModel channelPut;
         public static void Main(string[] args)
         {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqps://bublpzgh:M7fqfVEcZeB_LeBPkXanh8ZajkyGxfHq@snake.rmq2.cloudamqp.com/bublpzgh");
-            factory.AutomaticRecoveryEnabled = true;
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            try
             {
-                channel.QueueDeclare(queue: "hello",
+                initializationConnections(ConnectionSecret.GetConnectionFactory());
+                do
+                {
+                    listenPostQue();
+                    listenPutQue();
+
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($" Error {e.Message}");
+            }
+            finally
+            {
+                closeConnections();
+            }
+        }
+
+        private static void initializationConnections(ConnectionFactory factory)
+        {
+            connection = factory.CreateConnection();
+            channelPost = connection.CreateModel();
+            channelPut = connection.CreateModel();
+        }
+
+        private static void closeConnections()
+        {
+            connection?.Close();
+            channelPost?.Close();
+            channelPut?.Close();
+        }
+
+        private static void listenPostQue()
+        {
+            channelPost.QueueDeclare(queue: "Hello",
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                };
-                channel.BasicConsume(queue: "hello",
-                                     autoAck: true,
-                                     consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
+            var consumerPost = new AsyncEventingBasicConsumer(channelPost);
+            consumerPost.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                Console.WriteLine($"Answer from Client - {await WebApi.PostAsync("http://localhost:5500/api/Queue", message)}");
+            };
+            channelPost.BasicConsume(queue: "Hello",
+                                 autoAck: true,
+                                 consumer: consumerPost);
         }
+
+        private static void listenPutQue()
+        {
+            channelPost.QueueDeclare(queue: "Bye",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+            var consumerPost = new AsyncEventingBasicConsumer(channelPost);
+            consumerPost.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+                Console.WriteLine($"Answer from Client - {await WebApi.PutAsync("http://localhost:5500/api/Queue", message)}");
+            };
+            channelPost.BasicConsume(queue: "Bye",
+                                 autoAck: true,
+                                 consumer: consumerPost);
+        }
+          
     }
 
 
---
+++ ConsoleRabbitMQ.Receiver/WebApi.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRabbitMQ.Receiver
+{
+    public static class WebApi
+	{
+		public static async Task<string> PutAsync<T>(string api, T obj)
+		{
+			string result = string.Empty;
+			try
+			{
+				result = await GetPutResultAsync(api, obj);
+			}
+			catch (Exception exp)
+			{
+				Log.Warning(exp, $"Url: {api}");
+			}
+
+			return result;
+		}
+
+
+		public static async Task<string> PostAsync<T>(string api, T obj)
+		{
+			string result = string.Empty;
+			try
+			{
+				result = await GetPostResultAsync(api, obj);
+			}
+			catch (Exception exp)
+			{
+				Log.Warning(exp, $"Url: {api}");
+			}
+
+			return result;
+		}
+
+
+		private static async Task<string> GetPutResultAsync<T>(string api, T obj)
+		{
+			using (HttpClient client = new HttpClient(new HttpClientHandler { UseProxy = false, UseDefaultCredentials = true }))
+			{
+				var response = await client.PutAsync(api, new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json"));
+				try
+				{
+					response.EnsureSuccessStatusCode();
+					string result = await response.Content.ReadAsStringAsync();
+					return result;
+
+				}
+				catch (Exception exp)
+				{
+					Log.Warning(exp, $"Url: {api}");
+					return string.Empty;
+				}
+
+			}
+		}
+
+		private static async Task<string> GetPostResultAsync<T>(string api, T obj)
+		{
+			using (HttpClient client = new HttpClient(new HttpClientHandler { UseProxy = false, UseDefaultCredentials = true }))
+			{
+				var response = await client.PostAsync(api, new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json"));
+				try
+				{
+					response.EnsureSuccessStatusCode();
+					string result = await response.Content.ReadAsStringAsync();
+					return result;
+
+				}
+				catch(Exception exp)
+				{
+					Log.Warning(exp, $"Url: {api}");
+					return string.Empty;
+				}
+
+			}
+		}
+	}
+}
